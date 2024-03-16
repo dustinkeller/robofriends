9d814b95d4c4c432123e5890caa66c3e5d025f40
dustinkeller <dustin.w.keller@gmail.com>
2024-03-15 22:18:51 -0400

    added search logic, and multiple robots

---
+++ src/robots.js
@@ -0,0 +1,133 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+email: 'Rey.Padberg@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Alexa Rodriguez',
+    username: 'Alexus',
+    email: 'alexa@work.com'
+  },
+  {
+    id: 12,  
+    name: 'Michael Brown',
+    username: 'IronMike',
+    email: 'mike@factory.com'
+  },
+  {
+    id: 13,
+    name: 'Jessica Jones',
+    username: 'Jewel',
+    email: 'jessica@justice.net'
+  },
+  {
+    id: 14,
+    name: 'Tony Stark',
+    username: 'IronMan',
+    email: 'tony@starkindustries.com'
+  },
+  {
+    id: 15,
+    name: 'Bruce Banner',
+    username: 'Hulk',
+    email: 'bruce@science.edu'
+  },
+  {
+    id: 16,
+    name: 'Peter Parker',
+    username: 'SpiderMan',
+    email: 'peter@dailybugel.net'
+  },
+  {
+    id: 17,
+    name: 'Natasha Romanoff',
+    username: 'BlackWidow',
+    email: 'natasha@shield.gov'
+  },
+  {
+    id: 18,
+    name: 'Steve Rogers',
+    username: 'CaptainAmerica',
+    email: 'steve@shield.gov'
+  },
+  {
+    id: 19,
+    name: 'R2-D2',
+    username: 'Astromech',
+    email: 'beepboop@tatooine.com'
+  },
+  {
+    id: 20,
+    name: 'C-3PO',
+    username: 'Protocol',
+    email: 'protocol@goldenrod.com'
+  },
+  {
+    id: 21,
+    name: 'WALL-E',
+    username: 'WasteAllocation',
+    email: 'cleanup@earth.gov'
+  },
+  {
+    id: 22,
+    name: 'EVA',
+    username: 'Assistant',
+    email: 'assistant@discoveryone.com'
+  }];
